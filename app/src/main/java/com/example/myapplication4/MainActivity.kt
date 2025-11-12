61f84a5bad27876ded766b94194bb4a013b375e0
chltmdgh522 <chltmdgh517@naver.com>
2025-11-12 18:26:06 +0900

    초기 세팅명언 앱 만들기 소개 및 레이아웃 설정

--- app/src/main/java/com/example/myapplication4/MainActivity.kt
+++ app/src/main/java/com/example/myapplication4/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.myapplication4
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.showAllSentenceBtn.setOnClickListener {
+            val intent = Intent(this, SentenceActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
