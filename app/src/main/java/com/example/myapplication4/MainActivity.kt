c39f4f5333ad01be97f9da534269c8330cd7893b
chltmdgh522 <chltmdgh517@naver.com>
2025-11-12 18:31:26 +0900

    랜덤 명언 노출 기능 추가

--- app/src/main/java/com/example/myapplication4/MainActivity.kt
+++ app/src/main/java/com/example/myapplication4/MainActivity.kt
@@ -16,12 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sentenceList = mutableListOf<String>()
+        sentenceList.add("모아이 디그다")
+        sentenceList.add("우럭 디그다")
+        sentenceList.add("성괴 디그다")
+        sentenceList.add("아싸 디그다")
+        sentenceList.add("원조 디그다")
+        sentenceList.add("디콩 디그다")
+        sentenceList.add("빵빵 디그다")
+
+
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.showAllSentenceBtn.setOnClickListener {
             val intent = Intent(this, SentenceActivity::class.java)
             startActivity(intent)
         }
+
+        binding.goodWordTextArea.setText(sentenceList.random())
 
     }
 }
